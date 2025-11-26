6630a9da000d68de4cfb9785d7d6203b13d9a007
estradaone <143682258+estradaone@users.noreply.github.com>
2025-11-26 16:36:29 -0600

    Actualización parte 12

---
+++ enviarWhatsApp.js
@@ -0,0 +1,13 @@
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+    .create({
+        body: 'Your appointment is coming up on July 21 at 3PM',
+        from: 'whatsapp:+14155238886',
+        to: 'whatsapp:+5219932802311'
+    })
+    .then(message => console.log(message.sid))
+    .done();
